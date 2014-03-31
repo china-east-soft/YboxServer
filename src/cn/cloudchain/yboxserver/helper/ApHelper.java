7ce959cacc4392be3890557a6229e744d17e5f74
Lazzy <lazzy.nan@gmail.com>
2014-03-31 10:19:55 +0800

    add hls start/stop methods

--- src/cn/cloudchain/yboxserver/helper/ApHelper.java
+++ src/cn/cloudchain/yboxserver/helper/ApHelper.java
@@ -1,5 +1,15 @@
 package cn.cloudchain.yboxserver.helper;
 
+import android.os.SystemProperties;
+
 public class ApHelper {
+	
+	public static void startHls() {
+		SystemProperties.set("ctl.start", "hls_service");
+	}
+	
+	public static void endHls() {
+		SystemProperties.set("ctl.stop", "hls_service");
+	}
 
 }
